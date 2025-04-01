7b6f61eddc379b869b92c8daa98d6279cd933787
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-04-01 17:28:56 +0300

    fixed database connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
 const dbTestRoute = require('./DB/test');
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -10,7 +11,7 @@ app.set('layout', 'layout')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(expressLayouts)
-app.use('/db-test', dbTestRoute);
+app.use('/db',dbTestRoute);
 
 // const mysql = require('mysql2')
 
@@ -27,14 +28,14 @@ app.get('/admin', (req, res) => {
     res.render('admin', { adminView: true })
 })
 
-//app.listen(3002, () => {
-//    console.log('server started at local port 3002')
-//})
-// ^^ only for local testing ^^
+app.listen(3002, () => {
+    console.log('server started at local port 3002')
+})
+/*  ^^ only for local testing ^^ */
 
-app.listen(process.env.PORT || 3000, () => {
+/* app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port " + (process.env.PORT || 3000));
-  });
+  }); */
   
 
 module.exports = app; 
