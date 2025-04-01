dd3a4347d852123655bd45f82faf43771f48026d
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-04-01 17:51:53 +0300

    fixed prev commit

--- DB/test.js
+++ DB/test.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./db');
+
 router.get('/', async (req, res) => {
     try {
       const [rows] = await db.query('SELECT 1 + 1 AS result');
