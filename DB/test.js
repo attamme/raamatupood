24fb75418f4ccd3895f32e2529c3f348015e600c
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-04-01 17:58:51 +0300

    ashrasjh

--- DB/test.js
+++ DB/test.js
@@ -3,24 +3,25 @@ const router = express.Router();
 const db = require('./db');
 
 router.get('/', async (req, res) => {
-    try {
-      const [rows] = await db.query('SELECT 1 + 1 AS result');
-      res.send(`✅ DB works! 1 + 1 = ${rows[0].result}`);
-    } catch (err) {
-      console.error('❌ DB error:', {
-        message: err.message,
-        code: err.code,
-        errno: err.errno,
-        sqlMessage: err.sqlMessage,
-        stack: err.stack
-      });
-  
-      res.status(500).json({
-        message: err.message,
-        code: err.code,
-        errno: err.errno,
-        sqlMessage: err.sqlMessage
-      });
-    }
-  });
-  
+  try {
+    const [rows] = await db.query('SELECT 1 + 1 AS result');
+    res.send(`✅ DB works! 1 + 1 = ${rows[0].result}`);
+  } catch (err) {
+    console.error('❌ DB error:', {
+      message: err.message,
+      code: err.code,
+      errno: err.errno,
+      sqlMessage: err.sqlMessage,
+      stack: err.stack
+    });
+
+    res.status(500).json({
+      message: err.message,
+      code: err.code,
+      errno: err.errno,
+      sqlMessage: err.sqlMessage
+    });
+  }
+});
+
+module.exports = router;
