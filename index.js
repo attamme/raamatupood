c1724421269b9488053ef7c8602757e3e727516c
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-04-01 15:09:58 +0300

    reverted back

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('layout', 'layout')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(expressLayouts)
+
+// const mysql = require('mysql2')
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express deployed via Vercel!');
-});
+    res.render('index', {adminView: false})
+    console.log('server started at local')
+})
 
-module.exports = app;
+app.get('/login', (req, res) => {
+    res.render('login', { adminView: false })
+})
+
+app.get('/admin', (req, res) => {
+    res.render('admin', { adminView: true })
+})
+
+//app.listen(3002, () => {
+//    console.log('server started at local port 3002')
+//})
+// ^^ only for local testing ^^
+
+module.exports = app; // <== important for Vercel
