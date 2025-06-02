9d26b0e4ca5a4d45d71b66522b462f67909ade52
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-06-02 14:33:00 +0300

    send a message on contact page

--- routes/pages.js
+++ routes/pages.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => res.render('index', {adminView: false}))
 // router.get('/books', (req, res) => res.render('books', {adminView: false}))
 router.get('/author', (req, res) => res.render('authors', {adminView: false}))
 router.get('/contact', (req, res) => res.render('contact', {adminView: false}))
+router.post('/contact', async (req, res) => {
+    const { nimi, tekst } = req.body;
+    res.render('contact', { adminView: false, success: true, nimi})
+})
 router.get('/login', (req, res) => res.render('login', { adminView: false }))
 // router.get('/cart', (req, res) => res.render('cart', { adminView: false }))
 router.get('/order', (req, res) => res.render('order', { adminView: false }))
