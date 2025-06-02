ade8695e0e7848735c32f294a74c1e550c21c905
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-06-02 14:25:52 +0300

    order

--- routes/pages.js
+++ routes/pages.js
@@ -7,5 +7,9 @@ router.get('/author', (req, res) => res.render('authors', {adminView: false}))
 router.get('/contact', (req, res) => res.render('contact', {adminView: false}))
 router.get('/login', (req, res) => res.render('login', { adminView: false }))
 // router.get('/cart', (req, res) => res.render('cart', { adminView: false }))
+router.get('/order', (req, res) => res.render('order', { adminView: false }))
+router.post('/order', async (req, res) => {
+    res.render('order-success', { adminView: false})
+})
 
 module.exports = router;
